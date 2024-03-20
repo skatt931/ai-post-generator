5b961f759b9fa793e4e1394d7567a66160ba57b9
skatt931 <skatt931@gmail.com>
2024-03-20 16:59:15 +0100

    feat: add login button

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+
+  console.log(user);
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                width={50}
+                height={50}
+                className="rounded-full"
+              />
+            </div>
+            <div>
+              <p>Hi {user.name}!</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <Link href="/api/auth/logout">Logout</Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </p>
+    </div>
+  );
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,3 @@
+export default function TokenTopup() {
+  return <div>TokenTopup page</div>;
+}
